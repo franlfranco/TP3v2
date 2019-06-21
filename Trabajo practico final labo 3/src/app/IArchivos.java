d53a1e5ede7d594b82cee34ff50b88764f302efe
matiemanuel <43448876+matiemanuel@users.noreply.github.com>
2019-06-21 18:21:39 -0300

    archivos

---
+++ Trabajo practico final labo 3/src/app/IArchivos.java
@@ -0,0 +1,16 @@
+package app;
+
+
+public interface IArchivos {
+	
+	/**
+	 * Método para leer del archivo
+	 */
+	public void leerDeArchivo();
+	
+	/**
+	 * Método para cargar el archivo
+	 * @param <T>
+	 */
+	public void cargarArchivo();
+}
